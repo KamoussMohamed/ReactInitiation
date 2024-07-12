6d88295c517f08ea49a75265d61311cb42a2206e
Mohamed <mohamedkamouss@outlook.fr>
2024-07-12 11:39:19 +0100

    MY FIRST COMPONENT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
-class App extends React.Component{
-    render(){
-        return(
-            <div className='App'>
-                <h1>Hello from React</h1>
-            </div>
-        )
-    }
-}
 ReactDOM.render(<App/>, document.getElementById('root'))
