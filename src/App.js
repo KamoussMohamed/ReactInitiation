be63213e5274159b6e6e0093629bd431210f6635
Mohamed <mohamedkamouss@outlook.fr>
2024-07-12 11:45:58 +0100

    MY FIRST SIMPLE COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Table from './Table';
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
         return (
             <div className='App'>
                 <h1>Hello from App.js</h1>
+                <Table/>
             </div>
         )
     }
